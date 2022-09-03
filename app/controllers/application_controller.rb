fb22e53beb8e7d5ee4e5223cbdff4964d263f1c5
mnjoki21 <mitchellenjoki7@gmail.com>
2022-09-03 13:59:12 +0300

    using a web api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,20 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # get '/' do
+  #   { message: "Hello world" }.to_json
+  # end
+  set :default_content_type, 'application/json'
+
+   get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10)
+    # return a JSON response with an array of all the game data
+    games.to_json
   end
 
+  get 'games/:id' do
+    game = Game.find(params[:id])
+    game.to_json(include: :reviews)
+    
+  end
 end
